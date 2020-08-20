4a3271e523f4768983f4bb81007d816edd42c107
dinesh04bishnoi <61000017+dinesh04bishnoi@users.noreply.github.com>
2020-08-20 22:22:52 +0530

    push

--- secondpage.js
+++ secondpage.js
@@ -1,8 +1,23 @@
- document.getElementById("result").innerHTML = localStorage.getItem("Name");
-        document.getElementById("result2").innerHTML = localStorage.getItem("Address");
-        document.getElementById("result3").innerHTML = localStorage.getItem("Email");
-        document.getElementById("result4").innerHTML = localStorage.getItem("Phone No.");
-        document.getElementById("result5").innerHTML = localStorage.getItem("Hobbies");
-        document.getElementById("result6").innerHTML = "Studied :- "+localStorage.getItem("Level")+" In Year "+localStorage.getItem("Year")+" With Grade "+ localStorage.getItem("Grade")+"";
-        document.getElementById("result7").innerHTML = "You got :- "+localStorage.getItem("Skills")+" In your skills With Rating "+ localStorage.getItem("Rating")+"";
-        document.getElementById("result8").innerHTML = localStorage.getItem("Photo");
+var education=JSON.parse(localStorage.getItem('education'));
+var skills=JSON.parse(localStorage.getItem('Skill'));
+console.log(education);
+
+console.log(skills);
+
+document.getElementById("result").innerHTML = localStorage.getItem("Name");
+document.getElementById("result2").innerHTML = localStorage.getItem("Address");
+document.getElementById("result3").innerHTML = localStorage.getItem("Email");
+document.getElementById("result4").innerHTML = localStorage.getItem("Phone No.");
+document.getElementById("result5").innerHTML = localStorage.getItem("Hobbies");
+// document.getElementById("result6").innerHTML = "Studied :- " + localStorage.getItem("Level") + " In Year " + localStorage.getItem("Year") + " With Grade " + localStorage.getItem("Grade") + "";
+// document.getElementById("result7").innerHTML = "You got :- " + localStorage.getItem("Skills") + " In your skills With Rating " + localStorage.getItem("Rating") + "";
+education.forEach((element,index) => {
+    var html=`${index+1}:- Studied :- ${element.level} In Year ${element.year} With Grade ${element.grade}<br>`;
+    document.getElementById("result6").innerHTML+=html;
+});
+
+skills.forEach((element,index) => {
+    var html=`${index+1}:- You got :- ${element.skillName} in your skills with rating ${element.Rating}<br>`;
+    document.getElementById("result7").innerHTML+=html;
+});
+document.getElementById("result8").innerHTML = localStorage.getItem("Photo");
